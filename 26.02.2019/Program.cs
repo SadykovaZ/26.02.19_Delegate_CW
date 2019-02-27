c74e2a6f0848019dd49a3e66333c7bc79d294e74
СадыковаЖ <@205_8>
2019-02-27 11:11:04 +0600

    ++

--- 26.02.2019/Program.cs
+++ 26.02.2019/Program.cs
@@ -6,9 +6,32 @@ using System.Threading.Tasks;
 
 namespace _26._02._2019
 {
+    public class EventArgs
+    {
+        public static readonly System.EventArgs Empty;
+        public EventArgs()
+        {
+
+        }
+
+    }
+
+    public class CarEvents : EventArgs
+    {
+        public readonly string msg;
+        public CarEvents(string msg)
+        {
+            this.msg = msg;
+        }
+    }
+
+
     public delegate void CarEngineHandlet(string masgForCaller);
     public class Car
     {
+        public event CarEngineHandlet Exploded;
+        public event CarEngineHandlet AboutToBlow;
+
         public Car()
         {
             MaxtSpeed = 100;
@@ -27,31 +50,29 @@ namespace _26._02._2019
 
         private bool CarIsDead;        
 
-        private CarEngineHandlet listofHandlers;
+       // private CarEngineHandlet listofHandlers;
 
-        public void RegisterCarEngine(CarEngineHandlet methodCall)
-        {
-            listofHandlers += methodCall;
-        }
+        //public void RegisterCarEngine(CarEngineHandlet methodCall)
+        //{
+        //    listofHandlers += methodCall;
+        //}
 
         public void Accelerate(int delta)
         {
             if (CarIsDead)
             {
-                if (listofHandlers != null)
+                if (Exploded != null)
                 {
                     //listofHandlers("car is dead");
-                    listofHandlers.Invoke("Sorry car is dead");
-
-                }
-               
+                    Exploded.Invoke("Sorry car is dead");
+                }               
             }
             else
             {
                 this.CurrentSpeed += delta;
-                if (10 == (MaxtSpeed - CurrentSpeed) && listofHandlers != null)
+                if (10 == (MaxtSpeed - CurrentSpeed) && AboutToBlow != null)
                 {
-                    listofHandlers.Invoke("Careful");
+                    AboutToBlow.Invoke("Careful");
                 }
                 if (CurrentSpeed >= MaxtSpeed)
                 {
@@ -75,6 +96,12 @@ namespace _26._02._2019
     //1 Обьявление делегата
     public delegate void GetMessage();
     public delegate int Operation(int x, int y);
+
+    public delegate int Sum(int number);
+    public delegate int LengthLogin(string s);
+    public delegate bool BoolPassword(string s1, string s2);
+
+
     class Program
     {
         //2 Создаем переменную делегата
@@ -82,10 +109,45 @@ namespace _26._02._2019
         // static Operation op;
         static void Main(string[] args)
         {
+
+            setLogin();
+
+            return;
+            #region
+            CarEngineHandlet handler =
+               delegate (string mes)
+                //delegate (string mes)
+            {
+                Console.WriteLine("Message");
+                Console.WriteLine("--> {0}", mes);
+                Console.WriteLine("-------------------------");
+            };
+            handler("Ola");
+
+            ShowMessage("Hola", (string msg) => 
+            {
+                Console.WriteLine(msg);
+            });
+            #endregion
+            Sum del1 = SumValue();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Sum {0} equals: {1}",i, del1.Invoke(i));
+            }
+
+            #region
             Car car = new Car("Volvo", 100, 10);
-            car.RegisterCarEngine(onCarEngineEvent);
-            car.RegisterCarEngine(onCarEngineEvent2);
-            car.RegisterCarEngine(onCarEngineEvent3);
+
+            car.Exploded += handler;
+            //зарегестрировать событие
+            car.AboutToBlow += onCarEngineEvent;
+
+            CarEngineHandlet d = new CarEngineHandlet(onCarEngineEvent);
+            car.Exploded += d;
+
+            //car.RegisterCarEngine(onCarEngineEvent);
+            //car.RegisterCarEngine(onCarEngineEvent2);
+            //car.RegisterCarEngine(onCarEngineEvent3);
 
 
             for (int i = 0; i < 6; i++)
@@ -94,8 +156,8 @@ namespace _26._02._2019
             }
 
             Console.ReadLine();
-            
-            
+
+
             //if (DateTime.Now.Hour < 12)
             //{
             //    ShowMessage(GoodMorning);
@@ -133,8 +195,38 @@ namespace _26._02._2019
             //вызов метода
             //del();
             // del.Invoke();
+            #endregion
             Console.ReadLine();
 
+        }
+        static void setLogin()
+        {
+            string login = "15354";
+            LengthLogin lengthLoginDelegate = s => s.Length;
+            LengthLogin lengthLoginDelegate2 = (string s) => { return s.Length; };
+            //LengthLogin lengthLoginDelegate3 = (string s) => { return s.Length; };
+
+            //блочные лямба выражения
+            BoolPassword bp = (s1,s2) =>{ return true; };
+            bool res = bp.Invoke("", "");
+            Console.WriteLine(res);
+            int lengthLogin = lengthLoginDelegate(login);
+        }
+        static Sum SumValue()
+        {
+            int result = 0;
+
+            Sum del = (int number) =>
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    result += i;
+                }
+                return result;
+
+            };
+
+            return del;
         }
 
         private static void onCarEngineEvent(string msg)
@@ -180,6 +272,11 @@ namespace _26._02._2019
         private static void ShowMessage(GetMessage _del)
         {
             _del.Invoke();
+        }
+
+        private static void ShowMessage(string msg, CarEngineHandlet handler)
+        {
+            handler.Invoke(msg);
         }
 
     }
